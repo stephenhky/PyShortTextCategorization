934cb745537993353e527f23e68a05a986f358e4
stephenhky <stephenhky@yahoo.com.hk>
2016-11-03 12:20:15 -0400

    added C-LSTM implementation

--- nnlib/frameworks.py
+++ nnlib/frameworks.py
@@ -1,4 +1,4 @@
-from keras.layers import Convolution1D, MaxPooling1D, Flatten, Dense, Dropout
+from keras.layers import Convolution1D, MaxPooling1D, Flatten, Dense, Dropout, LSTM
 from keras.models import Sequential
 from keras.regularizers import l2
 
@@ -70,5 +70,34 @@ def DoubleCNNWordEmbed(nb_labels,
 # C-LSTM
 # Chunting Zhou, Chonglin Sun, Zhiyuan Liu, Francis Lau,
 # "A C-LSTM Neural Network for Text Classification", arXiv:1511.08630 (2015).
-def CLSTMWordEmbed(nb_labels):
-    pass
+def CLSTMWordEmbed(nb_labels,
+                   nb_filters=1200,
+                   n_gram=2,
+                   maxlen=15,
+                   vecsize=300,
+                   cnn_dropout=0.0,
+                   nb_rnnoutdim=1200,
+                   rnn_dropout=0.2,
+                   final_activation='softmax',
+                   dense_wl2reg=0.0
+                   ):
+    model = Sequential()
+    model.add(Convolution1D(nb_filter=nb_filters,
+                            filter_length=n_gram,
+                            border_mode='valid',
+                            activation='relu',
+                            input_shape=(maxlen, vecsize)))
+    if cnn_dropout > 0.0:
+        model.add(Dropout(cnn_dropout))
+    model.add(MaxPooling1D(pool_length=maxlen - n_gram + 1))
+    model.add(LSTM(nb_rnnoutdim))
+    if rnn_dropout > 0.0:
+        model.add(Dropout(rnn_dropout))
+    model.add(Dense(nb_labels,
+                    activation=final_activation,
+                    W_regularizer=l2(dense_wl2reg)
+                    )
+              )
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+    return model
