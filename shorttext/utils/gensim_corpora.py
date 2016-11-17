a9abeaf360815b413a3288a71f210a620c4ab67b
stephenhky <stephenhky@yahoo.com.hk>
2016-11-17 17:35:53 -0500

    bug fixed

--- shorttext/utils/gensim_corpora.py
+++ shorttext/utils/gensim_corpora.py
@@ -1,7 +1,7 @@
 import gensim
 from nltk import word_tokenize
 
-def generate_gensim_corpora(classdict, proprocess_and_tokenize=word_tokenize):
+def generate_gensim_corpora(classdict, preprocess_and_tokenize=word_tokenize):
     """ Generate gensim bag-of-words dictionary and corpus.
 
     Given a text data, a dict with keys being the class labels, and the values
@@ -10,12 +10,13 @@ def generate_gensim_corpora(classdict, proprocess_and_tokenize=word_tokenize):
 
     :param classdict: text data, a dict with keys being the class labels, and each value is a list of short texts
     :param proprocess_and_tokenize: preprocessor function, that takes a short sentence, and return a list of tokens (Default: `nltk.word_tokenize`)
-    :return: a tuple, consisting of a gensim dictionary and a corpus
+    :return: a tuple, consisting of a gensim dictionary, a corpus, and a list of class labels
     :type classdict: dict
-    :type proprocess_and_tokenize: function,
-    :rtype: (gensim.corpora.Dictionary, list)
+    :type proprocess_and_tokenize: function
+    :rtype: (gensim.corpora.Dictionary, list, list)
     """
-    doc = [proprocess_and_tokenize(classdict[classlabel]) for classlabel in sorted(classdict.keys())]
+    classlabels = sorted(classdict.keys())
+    doc = [preprocess_and_tokenize(' '.join(classdict[classlabel])) for classlabel in classlabels]
     dictionary = gensim.corpora.Dictionary(doc)
     corpus = [dictionary.doc2bow(doctokens) for doctokens in doc]
-    return dictionary, corpus
+    return dictionary, corpus, classlabels
