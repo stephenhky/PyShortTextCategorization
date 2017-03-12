3f7d55a94b4d9a851a6e89637346b7d5bc143ddc
stephenhky <cwin31>
2017-03-11 19:20:52 -0500

    added documentation for stacking

--- docs/tutorial_stacking.rst
+++ docs/tutorial_stacking.rst
@@ -1,12 +1,22 @@
 Stacked Generalization
 ======================
 
+"Stacking generates the members of the stacking ensemble using several learning algorithms and subsequently
+uses another algorithm to learn how to combine their outputs." It combines the classification results
+of several classifiers, and combines them.
+
+Stacking is most commonly implemented using logistic regression.
+Suppose there are *K* classifiers, and *l* output labels.
 
 
 Reference
 ---------
 
+"Combining the Best of All Worlds," *Everything About Data Analytics*, WordPress (2016). [`WordPress
+<https://datawarrior.wordpress.com/2016/06/19/combining-the-best-of-all-worlds/>`_]
+
 David H. Wolpert, "Stacked Generalization," *Neural Netw* 5: 241-259 (1992).
 
 M. Paz Sesmero, Agapito I. Ledezma, Araceli Sanchis, "Generating ensembles of heterogeneous classifiers using Stacked Generalization,"
 *WIREs Data Mining and Knowledge Discovery* 5: 21-34 (2015).
+
--- shorttext/stack/stacking.py
+++ shorttext/stack/stacking.py
@@ -8,12 +8,36 @@ import utils.classification_exceptions as e
 
 # abstract class
 class StackedGeneralization:
+    """
+    This is an abstract class for any stacked generalization method. It is an intermediate model
+    that takes the results of other classifiers as the input features, and perform another classification.
+
+    The classifiers must have the :func:`~score` method that takes a string as an input argument.
+
+    More references:
+
+    David H. Wolpert, "Stacked Generalization," *Neural Netw* 5: 241-259 (1992).
+
+    M. Paz Sesmero, Agapito I. Ledezma, Araceli Sanchis, "Generating ensembles of heterogeneous classifiers using Stacked Generalization,"
+    *WIREs Data Mining and Knowledge Discovery* 5: 21-34 (2015).
+    """
     def __init__(self, intermediate_classifiers={}):
+        """ Initialize the stacking class instance.
+
+        :param intermediate_classifiers: dictionary, with key being a string, and the values intermediate classifiers, that have the method :func:`~score`, which takes a string as the input argument.
+        :type intermediate_classifiers: dict
+        """
         self.classifiers = intermediate_classifiers
         self.classlabels = []
         self.trained = False
 
     def register_classifiers(self):
+        """ Register the intermediate classifiers.
+
+        It must be run before any training.
+
+        :return: None
+        """
         self.classifier2idx = {}
         self.idx2classifier = {}
         for idx, key in enumerate(self.classifiers.keys()):
@@ -21,20 +45,56 @@ class StackedGeneralization:
             self.idx2classifier[idx] = key
 
     def register_classlabels(self, labels):
+        """ Register output labels.
+
+        Given the labels, it gives an integer as the index for each label.
+        It is essential for the output model to place.
+
+        It must be run before any training.
+
+        :param labels: list of output labels
+        :return: None
+        :type labels: list
+        """
         self.classlabels = list(labels)
         self.labels2idx = {}
         for idx, classlabel in enumerate(self.classlabels):
             self.labels2idx[classlabel] = idx
 
     def add_classifier(self, name, classifier):
+        """ Add a classifier.
+
+        Add a classifier to the class. The classifier must have the method :func:`~score` which
+        takes a string as an input argument.
+
+        :param name: name of the classifier, without spaces and any special characters
+        :param classifier: instance of a classifier, which has a method :func:`~score` which takes a string as an input argument
+        :return: None
+        :type name: str
+        :type classifier: any class with a method :func:`~score`
+        """
         self.classifiers[name] = classifier
         self.register_classifiers()
 
     def delete_classifier(self, name):
+        """ Delete a classifier.
+
+        :param name: name of the classifier to be deleted
+        :return: None
+        :type name: str
+        :raise: KeyError
+        """
         del self.classifiers[name]
         self.register_classifiers()
 
     def translate_shorttext_intfeature_matrix(self, shorttext):
+        """ Represent the given short text as the input matrix of the stacking class.
+
+        :param shorttext: short text
+        :return: input matrix of the stacking class
+        :type shorttext: str
+        :rtype: numpy.ndarray
+        """
         feature_matrix = np.zeros((len(self.classifier2idx), len(self.labels2idx)))
         for key in self.classifier2idx:
             scoredict = self.classifiers[key].score(shorttext)
@@ -43,11 +103,31 @@ class StackedGeneralization:
         return feature_matrix
 
     def convert_label_to_buckets(self, label):
+        """ Convert the label into an array of bucket.
+
+        Some classification algorithms, especially those of neural networks, have the output
+        as a serious of buckets with the correct answer being 1 in the correct label, with other being 0.
+        This method convert the label into the corresponding buckets.
+
+        :param label: label
+        :return: array of buckets
+        :type label: str
+        :rtype: numpy.ndarray
+        """
         buckets = np.zeros(len(self.labels2idx), dtype=np.int)
         buckets[self.labels2idx[label]] = 1
         return buckets
 
     def convert_traindata_matrix(self, classdict, tobucket=True):
+        """ Returns a generator that returns the input matrix and the output labels for training.
+
+        :param classdict: dictionary of the training data
+        :param tobucket: whether to convert the label into buckets (Default: True)
+        :return: array of input matrix, and output labels
+        :type classdict: dict
+        :type tobucket: bool
+        :rtype: tuple
+        """
         for label in classdict:
             y = self.convert_label_to_buckets(label) if tobucket else self.labels2idx[label]
             for shorttext in classdict[label]:
@@ -55,13 +135,61 @@ class StackedGeneralization:
                 yield X, y
 
     def train(self, classdict, *args, **kwargs):
+        """ Train the stacked generalization.
+
+        Not implemented. `NotImplemntedException` raised.
+
+        :param classdict: training data
+        :param args: arguments to be parsed
+        :param kwargs: arguments to be parsed
+        :return: None
+        :type classdict: dict
+        :type args: dict
+        :type kwargs: dict
+        :raise: NotImplementedException
+        """
         raise e.NotImplementedException()
 
     def score(self, shorttext, *args, **kwargs):
+        """ Calculate the scores for each class labels.
+
+        Not implemented. `NotImplemntedException` raised.
+
+        :param shorttext: short text to be scored
+        :param args: arguments to be parsed
+        :param kwargs: arguments to be parsed
+        :return: dictionary of scores for all class labels
+        :type shorttext: str
+        :type args: dict
+        :type kwargs: dict
+        :rtype: dict
+        :raise: NotImplementedException
+        """
         raise e.NotImplementedException()
 
 class LogisticStackedGeneralization(StackedGeneralization):
+    """
+
+    This class implements logistic regression as the stacked generalizer.
+
+    It is an intermediate model
+    that takes the results of other classifiers as the input features, and perform another classification.
+
+    The classifiers must have the :func:`~score` method that takes a string as an input argument.
+    """
     def train(self, classdict, optimizer='adam', l2reg=0.01, nb_epoch=1000):
+        """ Train the stacked generalization.
+
+        :param classdict: training data
+        :param optimizer: optimizer to use Options: sgd, rmsprop, adagrad, adadelta, adam, adamax, nadam. (Default: 'adam', for adam optimizer)
+        :param l2reg: coefficients for L2-regularization (Default: 0.01)
+        :param nb_epoch: number of epochs for training (Default: 1000)
+        :return: None
+        :type classdict: dict
+        :type optimizer: str
+        :type l2reg: float
+        :type nb_epoch: int
+        """
         kmodel = Sequential()
         kmodel.add(Reshape((len(self.classifier2idx) * len(self.labels2idx),),
                            input_shape=(len(self.classifier2idx), len(self.labels2idx))))
@@ -82,6 +210,18 @@ class LogisticStackedGeneralization(StackedGeneralization):
         self.trained = True
 
     def score(self, shorttext):
+        """ Calculate the scores for all the class labels for the given short sentence.
+
+        Given a short sentence, calculate the classification scores for all class labels,
+        returned as a dictionary with key being the class labels, and values being the scores.
+        If the short sentence is empty, or if other numerical errors occur, the score will be `numpy.nan`.
+        If neither :func:`~train` nor :func:`~loadmodel` was run, it will raise `ModelNotTrainedException`.
+
+        :param shorttext: a short sentence
+        :return: a dictionary with keys being the class labels, and values being the corresponding classification scores
+        :type shorttext: str
+        :rtype: dict
+        """
         if not self.trained:
             raise e.ModelNotTrainedException()
 
