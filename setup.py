10fd1b111e44cc1a38d90ad483117ee5cfd44be1
stephenhky <stephenhky@yahoo.com.hk>
2017-07-03 22:20:37 -0400

    update version requirement in setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(name='shorttext',
       package_data={'shorttext': ['data/*.csv', 'utils/*.pkl']},
       setup_requires=['numpy'],
       install_requires=[
-          'numpy', 'scipy', 'scikit-learn', 'keras', 'gensim', 'pandas', 'spacy', 'stemming',
+          'numpy', 'scipy', 'scikit-learn', 'keras>=2.0.0', 'gensim>=2.2.0', 'pandas', 'spacy', 'stemming',
       ],
       scripts=['bin/ShortTextCategorizerConsole',
                'bin/ShortTextWord2VecSimilarity',
