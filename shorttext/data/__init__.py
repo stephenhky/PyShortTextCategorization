a4a575d696017c12b80e5afea065dacd36cd86e6
nmstoker <git@nmstoker.com>
2017-07-22 00:18:31 +0100

    Keep inaugual typo for backward compatibility

--- shorttext/data/__init__.py
+++ shorttext/data/__init__.py
@@ -1,1 +1,2 @@
 from .data_retrieval import subjectkeywords, nihreports, inaugural, retrieve_jsondata_as_dict, retrieve_csvdata_as_dict, yield_crossvalidation_classdicts
+from .data_retrieval import inaugural as inaugual
