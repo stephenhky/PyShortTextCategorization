995d67e406e89ebfeb6d52b11467673d5748a16f
stephenhky <stephenhky@yahoo.com.hk>
2018-01-08 17:29:46 -0500

    refactoring to reflect more general concepts

--- shorttext/generators/seq2seq/s2skeras.py
+++ shorttext/generators/seq2seq/s2skeras.py
@@ -6,28 +6,28 @@ from keras.layers import Input, LSTM, Dense
 # Reference: https://blog.keras.io/a-ten-minute-introduction-to-sequence-to-sequence-learning-in-keras.html
 
 class CharSeq2SeqWithKeras:
-    def __init__(self, nbchars, vecsize, latent_dim):
-        self.nbchars = nbchars
+    def __init__(self, nbelem, vecsize, latent_dim):
+        self.nbelem = nbelem
         self.vecsize = vecsize
         self.latent_dim = latent_dim
 
     def prepare_model(self):
         # Define an input sequence and process it.
-        encoder_inputs = Input(shape=(None, self.nbchars))
+        encoder_inputs = Input(shape=(None, self.nbelem))
         encoder = LSTM(self.latent_dim, return_state=True)
         encoder_outputs, state_h, state_c = encoder(encoder_inputs)
         # We discard `encoder_outputs` and only keep the states.
         encoder_states = [state_h, state_c]
 
         # Set up the decoder, using `encoder_states` as initial state.
-        decoder_inputs = Input(shape=(None, self.nbchars))
+        decoder_inputs = Input(shape=(None, self.nbelem))
         # We set up our decoder to return full output sequences,
         # and to return internal states as well. We don't use the
         # return states in the training model, but we will use them in inference.
         decoder_lstm = LSTM(self.latent_dim, return_sequences=True, return_state=True)
         decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
                                              initial_state=encoder_states)
-        decoder_dense = Dense(self.nbchars, activation='softmax')
+        decoder_dense = Dense(self.nbelem, activation='softmax')
         decoder_outputs = decoder_dense(decoder_outputs)
 
         # Define the model that will turn
