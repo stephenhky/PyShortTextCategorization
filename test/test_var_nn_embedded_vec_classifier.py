12cfb972eb31c014063545891a226253b93c271f
stephenhky <stephenhky@yahoo.com.hk>
2018-01-18 23:53:00 -0500

    updated test

--- test/test_var_nn_embedded_vec_classifier.py
+++ test/test_var_nn_embedded_vec_classifier.py
@@ -30,17 +30,17 @@ class TestVarNNEmbeddedVecClassifier(unittest.TestCase):
 		score_vals = main_classifier.score('artificial intelligence')
 		self.assertAlmostEqual(score_vals['mathematics'] + score_vals['physics'] + score_vals['theology'], 1.0, 1)
 
-    def testCNNWordEmbedWithGensim(self):
-		# create keras model using `CNNWordEmbed` class
-		keras_model = shorttext.classifiers.frameworks.CNNWordEmbed(wvmodel=self.w2v_model, nb_labels=len(self.trainclass_dict.keys()), vecsize=None, with_gensim=True)
-
-    	# create and train classifier using keras model constructed above
-		main_classifier = shorttext.classifiers.VarNNEmbeddedVecClassifier(self.w2v_model, with_gensim=True, vecsize=None)
-		main_classifier.train(self.trainclass_dict, keras_model, nb_epoch=2)
-
-		# compute classification score
-		score_vals = main_classifier.score('artificial intelligence')
-		self.assertAlmostEqual(score_vals['mathematics'] + score_vals['physics'] + score_vals['theology'], 1.0, 1)
+    # def testCNNWordEmbedWithGensim(self):
+		# # create keras model using `CNNWordEmbed` class
+		# keras_model = shorttext.classifiers.frameworks.CNNWordEmbed(wvmodel=self.w2v_model, nb_labels=len(self.trainclass_dict.keys()), vecsize=None, with_gensim=True)
+    #
+    # 	# create and train classifier using keras model constructed above
+		# main_classifier = shorttext.classifiers.VarNNEmbeddedVecClassifier(self.w2v_model, with_gensim=True, vecsize=None)
+		# main_classifier.train(self.trainclass_dict, keras_model, nb_epoch=2)
+    #
+		# # compute classification score
+		# score_vals = main_classifier.score('artificial intelligence')
+		# self.assertAlmostEqual(score_vals['mathematics'] + score_vals['physics'] + score_vals['theology'], 1.0, 1)
 
     def testDoubleCNNWordEmbedWithoutGensim(self):
   		# create keras model using `DoubleCNNWordEmbed` class
@@ -54,17 +54,17 @@ class TestVarNNEmbeddedVecClassifier(unittest.TestCase):
 		score_vals = main_classifier.score('artificial intelligence')
 		self.assertAlmostEqual(score_vals['mathematics'] + score_vals['physics'] + score_vals['theology'], 1.0, 1)
 
-    def testDoubleCNNWordEmbedWithGensim(self):
-		# create keras model using `DoubleCNNWordEmbed` class
-		keras_model = shorttext.classifiers.frameworks.DoubleCNNWordEmbed(wvmodel=self.w2v_model, nb_labels=len(self.trainclass_dict.keys()), vecsize=None, with_gensim=True)
-
-    	# create and train classifier using keras model constructed above
-		main_classifier = shorttext.classifiers.VarNNEmbeddedVecClassifier(self.w2v_model, with_gensim=True, vecsize=None)
-		main_classifier.train(self.trainclass_dict, keras_model, nb_epoch=2)
-
-		# compute classification score
-		score_vals = main_classifier.score('artificial intelligence')
-		self.assertAlmostEqual(score_vals['mathematics'] + score_vals['physics'] + score_vals['theology'], 1.0, 1)
+    # def testDoubleCNNWordEmbedWithGensim(self):
+		# # create keras model using `DoubleCNNWordEmbed` class
+		# keras_model = shorttext.classifiers.frameworks.DoubleCNNWordEmbed(wvmodel=self.w2v_model, nb_labels=len(self.trainclass_dict.keys()), vecsize=None, with_gensim=True)
+    #
+    # 	# create and train classifier using keras model constructed above
+		# main_classifier = shorttext.classifiers.VarNNEmbeddedVecClassifier(self.w2v_model, with_gensim=True, vecsize=None)
+		# main_classifier.train(self.trainclass_dict, keras_model, nb_epoch=2)
+    #
+		# # compute classification score
+		# score_vals = main_classifier.score('artificial intelligence')
+		# self.assertAlmostEqual(score_vals['mathematics'] + score_vals['physics'] + score_vals['theology'], 1.0, 1)
 
     def testCLSTMWordEmbedWithoutGensim(self):
   		# create keras model using `CLSTMWordEmbed` class
@@ -78,17 +78,17 @@ class TestVarNNEmbeddedVecClassifier(unittest.TestCase):
 		score_vals = main_classifier.score('artificial intelligence')
 		self.assertAlmostEqual(score_vals['mathematics'] + score_vals['physics'] + score_vals['theology'], 1.0, 1)
 
-    def testCLSTMWordEmbedWithGensim(self):
-  		# create keras model using `CLSTMWordEmbed` class
-  		keras_model = shorttext.classifiers.frameworks.CLSTMWordEmbed(wvmodel=self.w2v_model, nb_labels=len(self.trainclass_dict.keys()), vecsize=None, with_gensim=True)
-
-    	# create and train classifier using keras model constructed above
-		main_classifier = shorttext.classifiers.VarNNEmbeddedVecClassifier(self.w2v_model, with_gensim=True, vecsize=None)
-		main_classifier.train(self.trainclass_dict, keras_model, nb_epoch=2)
-
-		# compute classification score
-		score_vals = main_classifier.score('artificial intelligence')
-		self.assertAlmostEqual(score_vals['mathematics'] + score_vals['physics'] + score_vals['theology'], 1.0, 1)
+    # def testCLSTMWordEmbedWithGensim(self):
+  	# 	# create keras model using `CLSTMWordEmbed` class
+  	# 	keras_model = shorttext.classifiers.frameworks.CLSTMWordEmbed(wvmodel=self.w2v_model, nb_labels=len(self.trainclass_dict.keys()), vecsize=None, with_gensim=True)
+    #
+    # 	# create and train classifier using keras model constructed above
+		# main_classifier = shorttext.classifiers.VarNNEmbeddedVecClassifier(self.w2v_model, with_gensim=True, vecsize=None)
+		# main_classifier.train(self.trainclass_dict, keras_model, nb_epoch=2)
+    #
+		# # compute classification score
+		# score_vals = main_classifier.score('artificial intelligence')
+		# self.assertAlmostEqual(score_vals['mathematics'] + score_vals['physics'] + score_vals['theology'], 1.0, 1)
 
 if __name__ == '__main__':
     unittest.main()
