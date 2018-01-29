eb4801313fa0e53a0285651a9886ac5502171d0b
stephenhky <stephenhky@yahoo.com.hk>
2018-01-28 23:51:30 -0500

    docstring for char2vec.py

--- shorttext/generators/charbase/char2vec.py
+++ shorttext/generators/charbase/char2vec.py
@@ -10,19 +10,50 @@ from shorttext.utils.misc import textfile_generator
 
 
 class SentenceToCharVecEncoder:
-    def __init__(self, dictionary):
+    """ A class that facilitates one-hot encoding from characters to vectors.
+
+    """
+    def __init__(self, dictionary, signalchar='\n'):
+        """ Initialize the one-hot encoding class.
+
+        :param dictionary: a gensim dictionary
+        :param signalchar: signal character, useful for seq2seq models (Default: '\n')
+        :type dictionary: gensim.corpora.Dictionary
+        :type signalchar: str
+        """
         self.dictionary = dictionary
+        self.signalchar = signalchar
         numchars = len(self.dictionary)
         self.onehot_encoder = OneHotEncoder()
         self.onehot_encoder.fit(np.arange(numchars).reshape((numchars, 1)))
 
-    def calculate_prelim_vec(self, cor_sent):
+    def calculate_prelim_vec(self, sent):
+        """ Convert the sentence to a one-hot vector.
+
+        :param sent: sentence
+        :return: a one-hot vector, with each element the code of that character
+        :type sent: str
+        :rtype: numpy.array
+        """
         return self.onehot_encoder.transform(
-            np.array([self.dictionary.token2id[c] for c in cor_sent]).reshape((len(cor_sent), 1))
+            np.array([self.dictionary.token2id[c] for c in sent]).reshape((len(sent), 1))
         )
 
     def encode_sentence(self, sent, maxlen, startsig=False, endsig=False):
-        cor_sent = ('\n' if startsig else '') + sent[:min(maxlen, len(sent))] + ('\n' if endsig else '')
+        """ Encode one sentence to a sparse matrix, with each row the expanded vector of each character.
+
+        :param sent: sentence
+        :param maxlen: maximum length of the sentence
+        :param startsig: signal character at the beginning of the sentence (Default: False)
+        :param endsig: signal character at the end of the sentence (Default: False)
+        :return: matrix representing the sentence
+        :type sent: str
+        :type maxlen: int
+        :type startsig: bool
+        :type endsig: bool
+        :rtype: scipy.sparse.csc_matrix
+        """
+        cor_sent = (self.signalchar if startsig else '') + sent[:min(maxlen, len(sent))] + (self.signalchar if endsig else '')
         sent_vec = self.calculate_prelim_vec(cor_sent).tocsc()
         if sent_vec.shape[0] == maxlen + startsig + endsig:
             return sent_vec
@@ -32,6 +63,21 @@ class SentenceToCharVecEncoder:
                               dtype=np.float64)
 
     def encode_sentences(self, sentences, maxlen, sparse=True, startsig=False, endsig=False):
+        """ Encode many sentences into a rank-3 tensor.
+
+        :param sentences: sentences
+        :param maxlen: maximum length of one sentence
+        :param sparse: whether to return a sparse matrix (Default: True)
+        :param startsig: signal character at the beginning of the sentence (Default: False)
+        :param endsig: signal character at the end of the sentence (Default: False)
+        :return: rank-3 tensor of the sentences
+        :type sentences: list
+        :type maxlen: int
+        :type sparse: bool
+        :type startsig: bool
+        :type endsig: bool
+        :rtype: scipy.sparse.csc_matrix or numpy.array
+        """
         encode_sent_func = partial(self.encode_sentence, startsig=startsig, endsig=endsig, maxlen=maxlen)
         list_encoded_sentences = map(encode_sent_func, sentences)
         if sparse:
@@ -41,5 +87,12 @@ class SentenceToCharVecEncoder:
 
 
 def initSentenceToCharVecEncoder(textfile):
+    """ Instantiate a class of SentenceToCharVecEncoder from a text file.
+
+    :param textfile: text file
+    :return: an instance of SentenceToCharVecEncoder
+    :type textfile: file
+    :rtype: SentenceToCharVecEncoder
+    """
     dictionary = Dictionary(map(lambda line: [c for c in line], textfile_generator(textfile)))
     return SentenceToCharVecEncoder(dictionary)
