17f29faccf05fc126f4724a35c241ddb6e73d592
stephenhky <stephenhky@yahoo.com.hk>
2018-03-12 14:25:22 -0400

    rename procedure

--- test/test_charonehot.py
+++ test/test_charonehot.py
@@ -11,7 +11,7 @@ class TestCharOneHot(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def testBigTxt(self):
+    def test_BigTxt(self):
         chartovec_encoder = shorttext.generators.initSentenceToCharVecEncoder(urllib2.urlopen('http://norvig.com/big.txt', 'r'))
         self.assertEqual(93, len(chartovec_encoder.dictionary))
         self.assertEqual('\n', chartovec_encoder.signalchar)
