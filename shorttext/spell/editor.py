946038c84671a1bbdc170ea1b5a8f652e51c18e4
stephenhky <stephenhky@yahoo.com.hk>
2018-03-22 17:35:10 -0400

    modularize Norvig spell corrector

---
+++ shorttext/spell/editor.py
@@ -0,0 +1,13 @@
+
+def compute_set_edits1(word):
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    deletes = [L + R[1:] for L, R in splits if R]
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
+    replaces = [L + c + R[1:] for L, R in splits if R for c in letters]
+    inserts = [L + c + R for L, R in splits for c in letters]
+    return set(deletes + transposes + replaces + inserts)
+
+
+def compute_set_edits2(word):
+    return (e2 for e1 in compute_set_edits1(word) for e2 in compute_set_edits1(e1))
