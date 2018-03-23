62710f95970ef0ec9729cf4139e9aa1cc23314e3
stephenhky <stephenhky@yahoo.com.hk>
2018-03-22 20:07:46 -0400

    more test

--- test/test_fuzzylogic.py
+++ test/test_fuzzylogic.py
@@ -10,6 +10,10 @@ class TestFuzzyLogic(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test_similarity(self):
+        self.assertEqual(shorttext.metrics.dynprog.damerau_levenshtein('debug', 'deubg'), 1)
+        self.assertEqual(shorttext.metrics.dynprog.longest_common_prefix('debug', 'debuag'), 4)
+
     def test_jaccard(self):
         self.assertAlmostEqual(shorttext.metrics.dynprog.similarity('diver', 'driver'), 5./6.)
 
