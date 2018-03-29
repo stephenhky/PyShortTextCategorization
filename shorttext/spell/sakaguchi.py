ca24f7df07fc2ce00869216a9c8deb3caafe6d04
stephenhky <stephenhky@yahoo.com.hk>
2018-03-29 17:32:06 -0400

    start implementing

--- shorttext/spell/sakaguchi.py
+++ shorttext/spell/sakaguchi.py
@@ -3,9 +3,14 @@
 # Article: http://cs.jhu.edu/~kevinduh/papers/sakaguchi17robsut.pdf
 
 from . import SpellCorrector
+from shorttext.generators import SentenceToCharVecEncoder, initSentenceToCharVecEncoder
+from .binarize import SpellingToConcatCharVecEncoder, SCRNNBinarizer
 
 class SCRNNSpellCorrector(SpellCorrector):
-    def __init__(self):
+    def __init__(self, concatcharvec_encoder=None, charvec_encoder=None):
+        self.concatcharvec_encoder = SpellingToConcatCharVecEncoder() if concatcharvec_encoder==None else concatcharvec_encoder
+
+    def preprocess_vec(self, text):
         pass
 
     def train(self, text):
