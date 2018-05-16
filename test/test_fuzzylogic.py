fdf07165cc5954287024883a73de02e318bd1391
stephenhky <stephenhky@yahoo.com.hk>
2018-05-15 21:16:47 -0400

    import unittest2

--- test/test_fuzzylogic.py
+++ test/test_fuzzylogic.py
@@ -11,13 +11,11 @@ class TestFuzzyLogic(unittest.TestCase):
         pass
 
     def test_similarity(self):
-        pass
-        # self.assertEqual(shorttext.metrics.dynprog.damerau_levenshtein('debug', 'deubg'), 1)
-        # self.assertEqual(shorttext.metrics.dynprog.longest_common_prefix('debug', 'debuag'), 4)
+        self.assertEqual(shorttext.metrics.dynprog.damerau_levenshtein('debug', 'deubg'), 1)
+        self.assertEqual(shorttext.metrics.dynprog.longest_common_prefix('debug', 'debuag'), 4)
 
     def test_jaccard(self):
-        pass
-        # self.assertAlmostEqual(shorttext.metrics.dynprog.similarity('diver', 'driver'), 5./6.)
+        self.assertAlmostEqual(shorttext.metrics.dynprog.similarity('diver', 'driver'), 5./6.)
 
 if __name__ == '__main__':
     unittest.main()
