2e2765d8fbe9c2d6ecd21ad1518094f164ac3967
stephenhky <stephenhky@yahoo.com.hk>
2018-05-30 22:10:15 -0400

    using mock

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,15 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-# import sys
+import sys
 # import os
+import mock
+
+MOCK_MODULES = ['Cython', 'numpy', 'scipy', 'sklearn', 'tensorflow', 'keras', 'gensim',
+                'pandas', 'spacy', 'stemming', 'pulp', 'shorttext', 'shorttext.utils',
+                'shorttext.data']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
