248437f271ace4ba4f169a92e07a9312928c137c
stephenhky <stephenhky@yahoo.com.hk>
2018-06-30 19:19:01 -0400

    updated test_dtm to use new stemmer

--- test/test_dtm.py
+++ test/test_dtm.py
@@ -19,7 +19,7 @@ class TestDTM(unittest.TestCase):
         pipeline = [lambda s: re.sub('[^\w\s]', '', s),
                     lambda s: re.sub('[\d]', '', s),
                     lambda s: s.lower(),
-                    lambda s: ' '.join(map(shorttext.utils.textpreprocessing.stem, shorttext.utils.tokenize(s)))
+                    lambda s: shorttext.utils.textpreprocessing.stem_text
                     ]
         txtpreproceesor = shorttext.utils.text_preprocessor(pipeline)
 
