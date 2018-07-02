0a0ba779c120b062590de4b8256f138a0fef5d77
stephenhky <stephenhky@yahoo.com.hk>
2018-07-02 14:18:10 -0400

    correcting test_dtm

--- test/test_dtm.py
+++ test/test_dtm.py
@@ -19,7 +19,7 @@ class TestDTM(unittest.TestCase):
         pipeline = [lambda s: re.sub('[^\w\s]', '', s),
                     lambda s: re.sub('[\d]', '', s),
                     lambda s: s.lower(),
-                    lambda s: shorttext.utils.textpreprocessing.stemword(s)
+                    lambda s: ' '.join(map(shorttext.utils.textpreprocessing.stemword, shorttext.utils.tokenize(s)))
                     ]
         txtpreproceesor = shorttext.utils.text_preprocessor(pipeline)
 
