0b9156109e0a39cd44e9e909dc4dc02e8c607de8
stephenhky <stephenhky@yahoo.com.hk>
2018-07-09 12:29:26 -0400

    updated pandas calling

--- shorttext/data/data_retrieval.py
+++ shorttext/data/data_retrieval.py
@@ -135,14 +135,14 @@ def nihreports(txt_col='PROJECT_TITLE', label_col='FUNDING_ICs', sample_size=512
     classdict = defaultdict(lambda : [])
 
     for rowidx in np.random.randint(nb_data, size=min(nb_data, sample_size)):
-        label = nih.iloc[rowidx, label_col]
+        label = nih.iloc[rowidx, nih.columns.get_loc(label_col)]
         if label_col=='FUNDING_ICs':
             if label=='':
                 label = 'OTHER'
             else:
                 endpos = label.index(':')
                 label = label[:endpos]
-        classdict[label] += [nih.iloc[rowidx, txt_col]]
+        classdict[label] += [nih.iloc[rowidx, nih.columns.get_loc(txt_col)]]
 
     return dict(classdict)
 
