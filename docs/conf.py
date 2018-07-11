7c42d4d1ced6133cefe0d3f62962904b0318b107
stephenhky <stephenhky@yahoo.com.hk>
2018-07-11 12:15:18 -0400

    documentations removing API

--- docs/conf.py
+++ docs/conf.py
@@ -14,13 +14,13 @@
 
 import sys
 # import os
-import mock
-
-MOCK_MODULES = ['Cython', 'numpy', 'scipy', 'sklearn', 'tensorflow', 'keras', 'gensim',
-                'pandas', 'spacy', 'stemming', 'pulp', 'shorttext', 'shorttext.utils',
-                'shorttext.data']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
+# import mock
+#
+# MOCK_MODULES = ['Cython', 'numpy', 'scipy', 'sklearn', 'tensorflow', 'keras', 'gensim',
+#                 'pandas', 'spacy', 'PyStemmer', 'pulp', 'shorttext', 'shorttext.utils',
+#                 'shorttext.data']
+# for mod_name in MOCK_MODULES:
+#     sys.modules[mod_name] = mock.Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
