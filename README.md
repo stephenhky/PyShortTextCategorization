91f9045a26dcc7934b3d65f755c7664523f12f46
stephenhky <stephenhky@yahoo.com.hk>
2018-09-28 23:27:32 -0400

    rewritten required packages in setup

--- README.md
+++ README.md
@@ -111,7 +111,9 @@ If you would like to contribute, feel free to submit the pull requests. You can 
 ## Possible Future Updates
 
 - [ ] More scalability;
-- [ ] Less dependence on `pandas`;
+- [x] Less dependence on `pandas`;
+- [x] Update package requirements;
+- [ ] Dividing components to other packages;
 - [ ] Jupyter notebooks as tutorials;
 - [ ] Compatibility with Python 3.7;
 - [ ] More available corpus.
--- setup.py
+++ setup.py
@@ -69,8 +69,11 @@ setup(name='shorttext',
       include_dirs=[np.get_include()],
       setup_requires=['numpy>=1.11.3', 'scipy>=0.18.1'],
       install_requires=[
-          'Cython', 'numpy>=1.11.3', 'scipy>=0.18.1', 'scikit-learn', 'keras>=2.0.0', 'gensim>=3.2.0',
+          'Cython', 'numpy>=1.11.3', 'scipy>=0.18.1',
+          'keras_applications>=1.0.6', 'keras_preprocessing>=1.0.5',
+          'scikit-learn', 'keras>=2.0.0', 'gensim>=3.2.0',
           'pandas', 'spacy>=1.7.0', 'pulp', 'PyStemmer',
+
       ],
       tests_require=[
           'unittest2', 'keras>=2.0.0', 'gensim>=3.2.0', 'tensorflow',
