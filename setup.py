983e2e76cfbd870efd73298afbe61627b3e8a1de
stephenhky <stephenhky@yahoo.com.hk>
2018-09-29 00:35:25 -0400

    another fix for keras version

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(name='shorttext',
 
       ],
       tests_require=[
-          'unittest2', 'keras>=2.2.2', 'gensim>=3.2.0', 'tensorflow',
+          'unittest2', 'keras==2.2.2', 'gensim>=3.2.0', 'tensorflow',
       ],
       scripts=['bin/ShortTextCategorizerConsole',
                'bin/ShortTextWordEmbedSimilarity',
