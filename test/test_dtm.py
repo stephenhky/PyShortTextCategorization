4d6732d915caf86c192868ee858868d60db98fb5
Kwan-yuet Ho <stephenhky@yahoo.com.hk>
2020-04-07 22:15:12 -0400

    numbers updated (another)

--- test/test_dtm.py
+++ test/test_dtm.py
@@ -38,7 +38,7 @@ class TestDTM(unittest.TestCase):
         numdocs, numtokens = dtm.dtm.shape
         self.assertEqual(numdocs, 56)
         self.assertEqual(numtokens, 5406)
-        self.assertAlmostEqual(dtm.get_total_termfreq('government'), 0.27872964951168006,
+        self.assertAlmostEqual(dtm.get_total_termfreq('government'), 0.27584786568258396,
                                places=4)
 
 
