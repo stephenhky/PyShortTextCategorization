6e368f6606ef0eca6dd40d86d7e13e8fd3357cb2
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2020-09-02 12:18:46 -0400

    added autodoc extension

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.mathjax'
+    'sphinx.ext.mathjax', 'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
