10fd558ff21469abfbea1a38a3e625a561fd6ac1
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2020-09-02 18:20:33 -0400

    release 1.4.0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def test_requirements():
 
 
 setup(name='shorttext',
-      version='1.4.0a01',
+      version='1.4.0',
       description="Short Text Mining",
       long_description=package_description(),
       long_description_content_type='text/markdown',
