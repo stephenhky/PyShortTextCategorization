d7178663d9236822fdcba6cbe0292198e5b89d85
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2020-09-23 14:19:14 -0400

    testing if Python 3.9 passed

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def test_requirements():
 
 
 setup(name='shorttext',
-      version='1.4.1',
+      version='1.4.2a1',
       description="Short Text Mining",
       long_description=package_description(),
       long_description_content_type='text/markdown',
