64a6905e4a62a9283e6b70096fd6b538aa862e62
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2021-11-11 15:21:38 -0500

    new stop words list without negation terms, new modularization

--- test/test_textpreprocessing.py
+++ test/test_textpreprocessing.py
@@ -14,6 +14,10 @@ class TestTextPreprocessing(unittest.TestCase):
         preprocessor = shorttext.utils.standard_text_preprocessor_1()
         self.assertEqual(preprocessor('I love you.'), 'love')
         self.assertEqual(preprocessor('Natural language processing and text mining on fire.'), 'natur languag process text mine fire')
+        self.assertEqual(preprocessor('I do not think.'), 'think')
+
+    def testStandPipelineDifferentStopwords(self):
+        pass
 
 
 if __name__ == '__main__':
