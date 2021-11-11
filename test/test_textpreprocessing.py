f667265938192fe954ce5e3d6c83049cc8014ae4
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2021-11-11 15:30:24 -0500

    non-negative word list

--- test/test_textpreprocessing.py
+++ test/test_textpreprocessing.py
@@ -17,7 +17,10 @@ class TestTextPreprocessing(unittest.TestCase):
         self.assertEqual(preprocessor('I do not think.'), 'think')
 
     def testStandPipelineDifferentStopwords(self):
-        pass
+        preprocessor = shorttext.utils.standard_text_preprocessor_2()
+        self.assertEqual(preprocessor('I love you.'), 'love')
+        self.assertEqual(preprocessor('Natural language processing and text mining on fire.'), 'natur languag process text mine fire')
+        self.assertEqual(preprocessor('I do not think.'), 'not think')
 
 
 if __name__ == '__main__':
