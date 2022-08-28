8f0e1aa08706c98486e16f19c6c46792a21f1640
Ritesh Agrawal <ragrawal@gmail.com>
2022-08-28 07:02:14 -0700

    Bumping up version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def test_requirements():
 
 
 setup(name='shorttext',
-      version='1.5.5',
+      version='1.5.6',
       description="Short Text Mining",
       long_description=package_description(),
       long_description_content_type='text/markdown',
