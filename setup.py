e22d240f85f45cda0d1d36195efdaf300f183b5f
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2023-02-25 15:13:36 -0500

    retry

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def test_requirements():
 
 
 setup(name='shorttext',
-      version='1.5.8',
+      version='1.5.9a1',
       description="Short Text Mining",
       long_description=package_description(),
       long_description_content_type='text/markdown',
