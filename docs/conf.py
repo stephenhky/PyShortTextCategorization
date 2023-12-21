e183bac2a362051087e3cb9160ccd8f2ee67f315
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2023-12-21 16:09:05 -0500

    name change due to US naturalization

--- docs/conf.py
+++ docs/conf.py
@@ -48,8 +48,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'shorttext'
-copyright = u'2017, Kwan-Yuet Ho'
-author = u'Kwan-Yuet Ho'
+copyright = u'2017, Kwan Yuet Stephen Ho'
+author = u'Kwan Yuet Stephen Ho'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
