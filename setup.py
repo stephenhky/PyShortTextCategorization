dffaea4ca305f7b6f18617e255d4ea2f77234454
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2024-12-12 21:00:31 -0500

    use prebuilt levenshtein distance computation

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 
 from setuptools import setup
 import numpy as np
-from Cython.Build import cythonize
-
-ext_modules = cythonize(['shorttext/metrics/dynprog/dldist.pyx'])
 
 
 setup(
-      ext_modules=ext_modules,
       include_dirs=[np.get_include()],
       scripts=['bin/ShortTextCategorizerConsole',
                'bin/ShortTextWordEmbedSimilarity']
---
+++ shorttext/metrics/dynprog/dldist.py
@@ -0,0 +1,6 @@
+
+from Levenshtein import distance
+
+
+def damerau_levenshtein(word1, word2):
+    return distance(word1, word2)
