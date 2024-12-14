088d1bee5e63fe8437b4d177d757a0dc7b2cb726
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2024-12-13 22:09:55 -0500

    codes cleanup

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 
 from setuptools import setup
-import numpy as np
 
 
 setup(
-      include_dirs=[np.get_include()],
       scripts=['bin/ShortTextCategorizerConsole',
                'bin/ShortTextWordEmbedSimilarity']
       )
