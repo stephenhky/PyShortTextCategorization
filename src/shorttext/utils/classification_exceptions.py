1b9ab2a9410f1100151bf7d8b28fa69dbf986114
Kwan Yuet Stephen Ho <stephenhky@yahoo.com.hk>
2026-04-07 13:34:56 -0400

    type hinting to exception classes

--- src/shorttext/utils/classification_exceptions.py
+++ src/shorttext/utils/classification_exceptions.py
@@ -1,5 +1,9 @@
 
+from os import PathLike
+from pathlib import Path
+
 from deprecation import deprecated
+import numpy as np
 
 
 class ModelNotTrainedException(Exception):
@@ -8,17 +12,17 @@ class ModelNotTrainedException(Exception):
 
 
 class AlgorithmNotExistException(Exception):
-    def __init__(self, algoname):
+    def __init__(self, algoname: str):
         self.message = f"Algorithm {algoname} not exist."
 
 
 class WordEmbeddingModelNotExistException(Exception):
-    def __init__(self, path):
-        self.message = f"Given path of the word-embedding model not exist: {path}"
+    def __init__(self, path: str | PathLike):
+        self.message = f"Given path of the word-embedding model not exist: {path.as_posix() if isinstance(path, Path) else path}"
 
 
 class UnequalArrayLengthsException(Exception):
-    def __init__(self, arr1, arr2):
+    def __init__(self, arr1: np.ndarray | list, arr2: np.ndarray | list):
         self.message = f"Unequal lengths: {len(arr1)} and {len(arr2)}"
 
 
@@ -29,10 +33,10 @@ class NotImplementedException(Exception):
 
 
 class IncorrectClassificationModelFileException(Exception):
-    def __init__(self, expectedname, actualname):
+    def __init__(self, expectedname: str, actualname: str):
         self.message = f"Incorrect model (expected: {expectedname} ; actual: {actualname})"
 
 
 class OperationNotDefinedException(Exception):
-    def __init__(self, opname):
+    def __init__(self, opname: str):
         self.message = f"Operation {opname} not defined"
