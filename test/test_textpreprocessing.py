216d9cc44f7a61a04b57d1fa14f839107ebbb32b
Kwan Yuet Stephen Ho <stephenhky@yahoo.com.hk>
2026-04-08 12:34:42 -0400

    convert test_textpreprocessing.py using pytest purely

--- test/test_textpreprocessing.py
+++ test/test_textpreprocessing.py
@@ -1,21 +1,15 @@
 
-import unittest
-
 import shorttext
 
-class TestTextPreprocessing(unittest.TestCase):
-    def testStandardPipeline(self):
-        preprocessor = shorttext.utils.standard_text_preprocessor_1()
-        self.assertEqual(preprocessor('I love you.'), 'love')
-        self.assertEqual(preprocessor('Natural language processing and text mining on fire.'), 'natur languag process text mine fire')
-        self.assertEqual(preprocessor('I do not think.'), 'think')
 
-    def testStandPipelineDifferentStopwords(self):
-        preprocessor = shorttext.utils.standard_text_preprocessor_2()
-        self.assertEqual(preprocessor('I love you.'), 'love')
-        self.assertEqual(preprocessor('Natural language processing and text mining on fire.'), 'natur languag process text mine fire')
-        self.assertEqual(preprocessor('I do not think.'), 'not think')
+def test_textpreprocessing_standard_pipeline():
+    preprocessor = shorttext.utils.standard_text_preprocessor_1()
+    assert preprocessor('I love you.') == 'love'
+    assert preprocessor('Natural language processing and text mining on fire.') == 'natur languag process text mine fire'
+    assert preprocessor('I do not think.') == 'think'
 
-
-if __name__ == '__main__':
-    unittest.main()
+def test_textpreprocessing_standard_pipeline_stopwords():
+    preprocessor = shorttext.utils.standard_text_preprocessor_2()
+    assert preprocessor('I love you.') == 'love'
+    assert preprocessor('Natural language processing and text mining on fire.') == 'natur languag process text mine fire'
+    assert preprocessor('I do not think.') == 'not think'
