ab6c751fde4bb94879d239466070e775c3a610d6
Kwan Yuet Stephen Ho <stephenhky@yahoo.com.hk>
2026-04-14 09:15:56 -0400

    command line functions updated

--- src/shorttext/cli/wordembedsim.py
+++ src/shorttext/cli/wordembedsim.py
@@ -9,7 +9,7 @@ from ..utils import tokenize, load_word2vec_model, load_fasttext_model, load_poi
 from ..utils import shorttext_to_avgvec
 from ..metrics.wasserstein import word_mover_distance
 from ..metrics.dynprog.jaccard import soft_jaccard_score
-
+from ..utils.compute import cosine_similarity
 
 typedict = {
     'word2vec': load_word2vec_model,
@@ -34,11 +34,11 @@ def main():
     tokenize('Mogu is cute.')
 
     time0 = time.time()
-    print("Loading "+args.type+"   model: "+args.modelpath)
+    print(f"Loading {args.type}   model: {args.modelpath}")
     wvmodel = typedict[args.type](args.modelpath)
     time1 = time.time()
     end = False
-    print("... loading time: "+str(time1 - time0)+" seconds")
+    print(f"... loading time: {time1 - time0} seconds")
 
     while not end:
         sent1 = input('sent1> ')
@@ -48,8 +48,7 @@ def main():
             sent2 = input('sent2> ')
 
             # output results
-            print("Cosine Similarity = %.4f" % (1 - cosine(shorttext_to_avgvec(sent1, wvmodel), shorttext_to_avgvec(sent2, wvmodel))))
-            print("Word-embedding Jaccard Score Similarity = %.4f" % jaccardscore_sents(sent1, sent2, wvmodel))
-            print("Word Mover's Distance = %.4f" % word_mover_distance(tokenize(sent1), tokenize(sent2), wvmodel))
-            print("Soft Jaccard Score (edit distance) = %.4f" % soft_jaccard_score(tokenize(sent1), tokenize(sent2)))
-
+            print(f"Cosine Similarity = {cosine_similarity(shorttext_to_avgvec(sent1, wvmodel), shorttext_to_avgvec(sent2, wvmodel)):.4f}")
+            print(f"Word-embedding Jaccard Score Similarity = {jaccardscore_sents(sent1, sent2, wvmodel):.4f}")
+            print(f"Word Mover's Distance = {word_mover_distance(tokenize(sent1), tokenize(sent2), wvmodel):.4f}")
+            print(f"Soft Jaccard Score (edit distance) = {soft_jaccard_score(tokenize(sent1), tokenize(sent2)):.4f}")
